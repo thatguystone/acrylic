79ad7e665beee5fbc1b7c1ceb601fcdc0be9c56a
Andrew Stone <a@stoney.io>
2017-03-24 10:28:00 -0700

    http_fs: Fix lint errors

--- http_fs.go
+++ http_fs.go
@@ -2,10 +2,11 @@ package acrylic
 
 import "net/http"
 
-// FS builds on top of http.FileSystem, searching each FS for the requested file
-// before giving up.
+// MultiFS builds on top of http.FileSystem, searching each FS for the requested
+// file before giving up.
 type MultiFS []http.FileSystem
 
+// Open implements http.FileSystem
 func (mfs MultiFS) Open(name string) (f http.File, err error) {
 	for _, fs := range mfs {
 		f, err = fs.Open(name)
