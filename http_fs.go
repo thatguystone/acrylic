cd6642a065501571238c318261abed4cb12dea24
Andrew Stone <a@stoney.io>
2017-03-28 11:45:31 -0700

    http_fs: Add FileServer, which adds caching headers

--- http_fs.go
+++ http_fs.go
@@ -2,6 +2,27 @@ package acrylic
 
 import "net/http"
 
+// FileServer implements an http.Handler that adds caching headers that force
+// revalidation to all responses.
+func FileServer(root http.FileSystem) http.Handler {
+	return fileServer{
+		h: http.FileServer(root),
+	}
+}
+
+type fileServer struct {
+	h http.Handler
+}
+
+func (h fileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	setCacheHeaders(w)
+	h.h.ServeHTTP(w, r)
+}
+
+func setCacheHeaders(w http.ResponseWriter) {
+	w.Header().Set("Cache-Control", "max-age=0, must-revalidate")
+}
+
 // MultiFS builds on top of http.FileSystem, searching each FS for the requested
 // file before giving up.
 type MultiFS []http.FileSystem
--- http_fs_test.go
+++ http_fs_test.go
@@ -2,10 +2,28 @@ package acrylic
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/thatguystone/cog/check"
 )
+
+func TestFileServerBasic(t *testing.T) {
+	c := check.New(t)
+
+	fs, clean := c.FS()
+	defer clean()
+
+	fs.SWriteFile("/file", "")
+	h := FileServer(http.Dir(fs.Path("/")))
+
+	w := httptest.NewRecorder()
+	h.ServeHTTP(w, httptest.NewRequest("GET", "/file", nil))
+
+	c.Equal(w.Code, 200)
+	c.Contains(w.HeaderMap.Get("Cache-Control"), "max-age=0")
+	c.Contains(w.HeaderMap.Get("Cache-Control"), "must-revalidate")
+}
 
 func TestMultiFSBasic(t *testing.T) {
 	c := check.New(t)
